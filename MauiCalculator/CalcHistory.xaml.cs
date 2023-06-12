9321629b34101f8d13d2e8febdcdc660f6d5cc22
SuriRose <surie680@gmail.com>
2023-06-11 21:16:52 -0400

    #63

---
+++ MauiCalculator/CalcHistory.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiCalculator;
+
+public partial class CalcHistory : ContentPage
+{
+	public CalcHistory()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiCalculator/Calculator.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiCalculator;
+
+public partial class Calculator : ContentPage
+{
+	public Calculator()
+	{
+		InitializeComponent();
+	}
+}
